13204ce8370502b015c19f58a018e74e6cf2777f
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2020-08-01 22:26:25 +0100

    updated post

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 // import './App.css';
 import Header from './components/header/Header';
+import Postlist from './components/PostList/PostList';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
         <Header/>
+        <Postlist/>
       </div>
     </div>
   );
--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -5,7 +5,8 @@ import './PostList.css'
 export default class Postlist extends Component {
     render () {
         return (
-            <div className="picture">
+            <div className="Post">
+                <div className="postListTop">
                 <div className="card">
                     <img src={`/images/lamb.jpg`} alt=""/>
                     <div className="card-text">
@@ -28,6 +29,32 @@ export default class Postlist extends Component {
                         <h4>Find or provide accomodations for covid-19 responders.</h4>
                     </div>
                 </div>
+                </div>
+                <div className="postListBottom">
+                <div className="card">
+                    <img src={`/images/lamb.jpg`} alt=""/>
+                    <div className="card-text">
+                        <h3>Online Experiences</h3>
+                        <h4>Unique activities we can do together, led by world of hosts.</h4>
+                    </div>
+                   
+                </div>
+                <div className="card">
+                    <img src={'/images/eating.jpg'} alt=""/>
+                    <div className="card-text">
+                        <h3>Monthly Stay</h3>
+                        <h4>Make airbnb your home, for stays of a month or longer.</h4>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={'/images/sheep.jpg'} alt=""/>
+                    <div className="card-text">
+                        <h3>Frontline Stay</h3>
+                        <h4>Find or provide accomodations for covid-19 responders.</h4>
+                    </div>
+                </div>
+                </div>
+                
             </div>
        )
     }
