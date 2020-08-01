8f76d9db3dadf0c091cf3e15fec673c256e95133
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2020-08-01 17:39:18 +0100

    header ja and header css

---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Header.Styles.css';
+
+
+export default class Header extends Component {
+    render () {
+        return (
+        
+            <div className="headerContainer">
+                <div className="headerContainerFlex">
+                <div className="headerLogo">
+                    <img src={`../images/edenlogo.svg`}/>
+                </div>
+                <div className="searchContainer">
+                    <form className="headerSearch">
+                        <input type="text" placeholder="SEARCH..." name="search"/>
+                        <button type="submit">GO</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        )
+    }
+};
