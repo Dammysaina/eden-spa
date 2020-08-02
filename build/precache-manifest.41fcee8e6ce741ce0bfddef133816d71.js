83d402beac6ee8e1ec1ec73c9f84d4b434c1ee01
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2020-08-02 10:32:53 +0100

    psot image update

---
+++ build/precache-manifest.41fcee8e6ce741ce0bfddef133816d71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae363a835db3502f9e8319b1a43a34d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcd7e3bcd9d10c5987db",
+    "url": "/static/css/main.da6b9fc5.chunk.css"
+  },
+  {
+    "revision": "6d4fa33e3386560accd0",
+    "url": "/static/js/2.41b41b7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41b41b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcd7e3bcd9d10c5987db",
+    "url": "/static/js/main.da885935.chunk.js"
+  },
+  {
+    "revision": "48e5d17772323149fe46",
+    "url": "/static/js/runtime-main.041de7c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
