4e68930ca31b418c1c2e5b2dcdde096c2a3410c3
Oluwadamilola Adediran <47598416+Dammysaina@users.noreply.github.com>
2020-08-02 19:45:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './redux';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ReduxProvider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ReduxProvider>,
   document.getElementById('root')
 );
 
